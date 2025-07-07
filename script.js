b3dc0d10df68b284d5fc08c874b377fcdc9166d5
Mohsendg <135234807+Mohsendg@users.noreply.github.com>
2025-07-07 21:12:56 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$(function () {
+  $("#flipbook").turn({
+    width: 1000,       // هم‌خوان با اندازه عکسهات
+    height: 750,
+    autoCenter: true,
+    elevation: 50,
+    gradients: true
+  });
+});
